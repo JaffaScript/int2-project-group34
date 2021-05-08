467376df5adbd66f3e003f6885e030dee955f98f
Harley <50149947+harley-rm@users.noreply.github.com>
2021-05-08 03:34:46 +0100

    Moved Net class to seperate file for readability.

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ import torch.backends.cudnn as cudnn
 import torchvision
 import torchvision.transforms as transforms
 
+from net import Net
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("Device: %s" % device)
 
@@ -41,24 +43,7 @@ loader_testing = torch.utils.data.DataLoader(dataset_testing, batch_size=bsize, 
 print("** LOADING COMPLETE! **\n")
 print("** Building net model.. **")
 # DESIGN NET MODEL #
-class Net(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
 
 net = Net()
 net.to(device)
