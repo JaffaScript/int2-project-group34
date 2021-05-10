068daaa7c55660fbb3eca19a430936fc0042ed1c
JaffaScript <65352576+JaffaScript@users.noreply.github.com>
2021-05-10 15:01:19 +0100

    model changes

--- main.py
+++ main.py
@@ -12,35 +12,104 @@ import numpy as np
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.shape = True
+        # Constraints for layer 1
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=5, stride=1, padding=2)
+        self.batch1 = nn.BatchNorm2d(16)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
 
-        # self.layer1 = nn.Sequential(
-        #     nn.Conv2d(3, 32, kernel_size=5, stride=1, padding=2),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d(kernel_size=2, stride=2)
-        # )
+        # Constraints for layer 2
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=2)
+        self.batch2 = nn.BatchNorm2d(32)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+
+        # Constraints for layer 3
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2)
+        self.batch3 = nn.BatchNorm2d(64)
+        self.relu3 = nn.ReLU()
+        self.pool3 = nn.MaxPool2d(kernel_size=2)
+
+        # Constraints for layer 4
+        self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, stride=1, padding=2)
+        self.batch4 = nn.BatchNorm2d(128)
+        self.relu4 = nn.ReLU()
+        self.pool4 = nn.MaxPool2d(kernel_size=2)
+
+        # Constraints for layer 5
+        self.conv5 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=5, stride=1, padding=2)
+        self.batch5 = nn.BatchNorm2d(256)
+        self.relu5 = nn.ReLU()
+        self.pool5 = nn.MaxPool2d(kernel_size=2)
+
+        # Defining the Linear layer
+        self.fc = nn.Linear(128 * 2 * 2, 500)
+        self.fc2 = nn.Linear(500, 100)
+        self.fc3 = nn.Linear(100, 10)
 
     def forward(self, x):
-        #x = self.layer1(x)
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        if self.shape:
+            print("ORIG " + str(x.shape))
+
+        # Conv 1
+        out = self.conv1(x)
+        out = self.batch1(out)
+        out = self.relu1(out)
+        out = self.pool1(out)
+
+        if self.shape:
+            print("1 " + str(out.shape))
+
+        # Conv 2
+        out = self.conv2(out)
+        out = self.batch2(out)
+        out = self.relu2(out)
+        out = self.pool2(out)
+
+        if self.shape:
+            print("2 " + str(out.shape))
+
+        # Conv 3 
+        out = self.conv3(out)
+        out = self.batch3(out)
+        out = self.relu3(out)
+        out = self.pool3(out)
+
+        if self.shape:
+            print("3 " + str(out.shape))
+
+        # Conv 4 
+        out = self.conv4(out)
+        out = self.batch4(out)
+        out = self.relu4(out)
+        out = self.pool4(out)
+
+        if self.shape:
+            print("4 " + str(out.shape))
+
+        # Fitting
+        out = out.view(out.size(0), -1)
+
+        if self.shape:
+            print("6 " + str(out.shape))
+
+        # Linear Layer
+        out = self.fc(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+
+        if self.shape:
+            print("7 " + str(out.shape))
+            self.shape = False
+
+        return out
 
 def main():
-
     # Defining hyperparameters
     num_epochs = int(input("Enter number of cyles through the dataset: "))
-    batch_size = 4
-    learning_rate = 0.001
+    batch_size = 128
+    learning_rate = 0.005
 
     # Defining device (GPU)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -70,6 +139,7 @@ def main():
     # Defining criteria and optimiser
     # Since this is a multiclass classification problem, we use CrossEntropyLoss function
     criterion = nn.CrossEntropyLoss()
+    #optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
     optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
 
     ###### BEGIN TRAINING ######
@@ -91,7 +161,7 @@ def main():
 
             # print statistics
             running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
+            if i % 150 == 149:    # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
                     (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
@@ -114,14 +184,14 @@ def main():
 
         for data in testing_dataloader:
             images, labels = data[0].to(device), data[1].to(device)
-            
+
             outputs = model(images)
 
             _, predicted = torch.max(outputs, 1)
             num_samples += labels.size(0)
             num_correct += (predicted == labels).sum().item()
 
-            for i in range(batch_size):
+            for i in range(4):
                 label = labels[i]
                 prediction = predicted[i]
                 if (label == prediction):
