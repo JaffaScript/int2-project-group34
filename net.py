6aff445534c8649987fb2328cb4927daa65ddcdc
Harley <50149947+harley-rm@users.noreply.github.com>
2021-05-10 12:26:42 +0100

    Testing new cnn

--- net.py
+++ net.py
@@ -7,25 +7,23 @@ class Net(nn.Module):
         super().__init__()
 
         self.conv1 = nn.Conv2d(3, 6, 3)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv3 = nn.Conv2d(16, 64, 2)
-        self.conv4 = nn.Conv2d(64, 75, 4)
-        self.pool2 = nn.MaxPool2d(3, 3)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.conv3 = nn.Conv2d(16, 64, 3, padding=1)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv4 = nn.Conv2d(64, 128, 1)
+        self.conv5 = nn.Conv2d(128, 192, 5, padding=1)
+        self.pool2 = nn.MaxPool2d(3, 2)
+        self.conv6 = nn.Conv2d(192, 256, 6, padding=1)
 
-        self.fc1 =  nn.Linear(75*3*3, 350)
-        self.fc2 = nn.Linear(350, 250)
-        self.fc3 = nn.Linear(250, 100)
-        self.fc4 = nn.Linear(100, 50)
-        self.fc5 = nn.Linear(50, 10)
+        self.fc1 = nn.Linear(250*2*2, 500)
+        self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv2(F.relu(self.conv1(x)))))
-        x = self.pool2(F.relu(self.conv4(F.relu(self.conv3(x)))))
-        x = x.view(-1, 75*3*3)
+        x = self.pool1(F.relu(self.conv3(F.relu(self.conv2(F.relu(self.conv1(x)))))))
+        x = self.pool2(F.relu(self.conv5(F.relu(self.conv4(x)))))
+        x = F.relu(self.conv6(x))
+
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.relu(self.fc4(x))
-        x = self.fc5(x)
+        x = self.fc2(x)
+
         return x
