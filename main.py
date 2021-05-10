71166831524f82ec14393c9190c2dd952f7550c1
Harley <50149947+harley-rm@users.noreply.github.com>
2021-05-10 12:08:06 +0100

    Added measure of accuracy on training images to help identify overfitting.

--- main.py
+++ main.py
@@ -117,3 +117,13 @@ with torch.no_grad():
         correct += (predicted == labels).sum().item()
 
 print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+
+with torch.no_grad():
+    for data in loader_training:
+        images, labels = data[0].to(device), data[1].to(device)
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        
+print('Accuracy of the network on the training images: %d %%' % (100 * correct / total))
